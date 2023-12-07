c1525a8c9f604afa75be9eecf2672107ce73f115
Sofi44444 <147055158+Sofi44444@users.noreply.github.com>
2023-12-07 18:09:07 -0300

    Add files via upload

---
+++ sorteo de alumnos.py
@@ -0,0 +1,8 @@
+import radom
+alumnos5C=random.randint()
+print('este es el sorteo de alumnos 5C')
+
+alumnos5C=['sofi','sabri','giane','nahi','cele','facu','mati','juan','valen']
+random.choice[alumnos5C]
+  
+  
